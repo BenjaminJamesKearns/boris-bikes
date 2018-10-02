d7931530d54d4ac95fd0e830efef3b61b46eb2ca
EC2 Default User <ec2-user@ip-172-31-28-201.us-east-2.compute.internal>
2018-10-02 15:13:54 +0000

    User can release and check bike - Step 10

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,5 +1,8 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it {expect(subject).to respond_to(:release_bike)}
+  it {bike = subject.release_bike 
+      expect(bike).to be_working}
 end
