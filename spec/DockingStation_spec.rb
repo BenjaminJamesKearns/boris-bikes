b97817f2d988df062332fe49be15680dfc3ee1e4
Benjamin Kearns <rapiidz1@gmail.com>
2018-10-03 14:09:36 +0000

    user can dock bike and see a docked bike

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -2,7 +2,23 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
-  it {expect(subject).to respond_to(:release_bike)}
-  it {bike = subject.release_bike 
-      expect(bike).to be_working}
+  it "checks release_bike method" do # one-line method: it {expect(subject).to respond_to(:release_bike)}
+    is_expected.to respond_to(:release_bike)
+  end
+  it "checks the bike is working" do 
+    bike = subject.release_bike 
+      expect(bike).to be_working
+    end
+  it "dock a bike in a station" do
+    bike = Bike.new 
+    expect(subject.dock(bike)).to eq bike
+  end
+  it "responds to .bike method" do
+    is_expected.to respond_to(:bike)
+  end
+  it "looks at bikes that are docked" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike 
+  end
 end
