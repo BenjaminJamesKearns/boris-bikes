38105a7551efb8cdb26791e5ebf4fda6eceb561f
Benjamin Kearns <rapiidz1@gmail.com>
2018-10-03 15:28:38 +0000

    added an error when docking station is empty

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -6,9 +6,9 @@ describe DockingStation do
     is_expected.to respond_to(:release_bike)
   end
   it "checks the bike is working" do 
-    bike = subject.release_bike 
-      expect(bike).to be_working
-    end
+    bike = Bike.new 
+    expect(bike).to be_working
+  end
   it "dock a bike in a station" do
     bike = Bike.new 
     expect(subject.dock(bike)).to eq bike
@@ -20,5 +20,15 @@ describe DockingStation do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike 
+  end
+  describe '#release_bike' do
+    it "releases a bike" do 
+      bike = Bike.new 
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike 
+    end
+    it "raises an error when there are no bikes available" do 
+      expect{subject.release_bike}.to raise_error 'No bikes available'
+    end
   end
 end
