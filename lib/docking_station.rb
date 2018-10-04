3c729828d8c6cdf950ac796ae6198ae1f0771633
Benjamin Kearns <rapiidz1@gmail.com>
2018-10-04 14:33:19 +0000

    put DockingStation capacity number into a constant

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,7 @@
 require_relative 'bike'
 
 class DockingStation
+  DEFAULT_CAPACITY = 20
   def initialize
     @bikes = []
   end
@@ -15,7 +16,7 @@ class DockingStation
   attr_reader :bikes
   private
   def full?
-    fail 'Docking station full' if @bikes.count >= 20
+    fail 'Docking station full' if @bikes.count >= DEFAULT_CAPACITY
   end
   def empty?
     fail 'No bikes available' if @bikes.empty?
